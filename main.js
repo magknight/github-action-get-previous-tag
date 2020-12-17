b093b3656a908e3c9575b1f5c04d865add6f4981
Alex Beavil <alex@aeroniemi.com>
2020-12-17 00:03:26 +0000

    fetch all the tags

--- main.js
+++ main.js
@@ -1,39 +1,46 @@
 const { exec } = require('child_process');
 const core = require('@actions/core');
 
-exec(`cd ${core.getInput('path')} && git rev-list --tags --max-count=1`, (err, rev, stderr) => {
+exec(`cd ${core.getInput('path')} && git fetch --tags --prune --unshallow -f`, (err, res, stderr) => {
     if (err) {
-        console.log('\x1b[33m%s\x1b[0m', 'Could not find any revisions because: ');
+        console.log('\x1b[33m%s\x1b[0m', `Couldn't fetch`);
         console.log('\x1b[31m%s\x1b[0m', stderr);
         process.exit(1);
     }
-
-    rev = rev.trim()
-
-    exec(`cd ${core.getInput('path')} && git describe --tags ${rev}`, (err, tag, stderr) => {
+    exec(`cd ${core.getInput('path')} && git rev-list --tags --max-count=1`, (err, rev, stderr) => {
         if (err) {
-            console.log('\x1b[33m%s\x1b[0m', rev)
-            console.log('\x1b[33m%s\x1b[0m', 'Could not find any tags because: ');
+            console.log('\x1b[33m%s\x1b[0m', 'Could not find any revisions because: ');
             console.log('\x1b[31m%s\x1b[0m', stderr);
             process.exit(1);
         }
 
-        tag = tag.trim()
+        rev = rev.trim()
 
-        exec(`cd ${core.getInput('path')} && git log -1 --format=%at ${tag}`, (err, timestamp, stderr) => {
+        exec(`cd ${core.getInput('path')} && git describe --tags ${rev}`, (err, tag, stderr) => {
             if (err) {
-                console.log('\x1b[33m%s\x1b[0m', 'Could not find any timestamp because: ');
+                console.log('\x1b[33m%s\x1b[0m', rev)
+                console.log('\x1b[33m%s\x1b[0m', 'Could not find any tags because: ');
                 console.log('\x1b[31m%s\x1b[0m', stderr);
                 process.exit(1);
             }
 
-            timestamp = timestamp.trim()
+            tag = tag.trim()
 
-            console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
-            console.log('\x1b[32m%s\x1b[0m', `Found timestamp: ${timestamp}`);
-            console.log(`::set-output name=tag::${tag}`);
-            console.log(`::set-output name=timestamp::${timestamp}`);
-            process.exit(0);
+            exec(`cd ${core.getInput('path')} && git log -1 --format=%at ${tag}`, (err, timestamp, stderr) => {
+                if (err) {
+                    console.log('\x1b[33m%s\x1b[0m', 'Could not find any timestamp because: ');
+                    console.log('\x1b[31m%s\x1b[0m', stderr);
+                    process.exit(1);
+                }
+
+                timestamp = timestamp.trim()
+
+                console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
+                console.log('\x1b[32m%s\x1b[0m', `Found timestamp: ${timestamp}`);
+                console.log(`::set-output name=tag::${tag}`);
+                console.log(`::set-output name=timestamp::${timestamp}`);
+                process.exit(0);
+            });
         });
     });
 });
