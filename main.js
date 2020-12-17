df71dc22032489d2a21b2d3bf5b385e3fd846860
Alex Beavil <alex@aeroniemi.com>
2020-12-17 00:32:48 +0000

    no message

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 const core = require('@actions/core');
 
-exec(`cd ${core.getInput('path')} && git fetch --tags --prune --unshallow -f`, (err, res, stderr) => {
+exec(`cd ${core.getInput('path')} && git fetch --tags g-f`, (err, res, stderr) => {
     if (err) {
         console.log('\x1b[33m%s\x1b[0m', `Couldn't fetch`);
         console.log('\x1b[31m%s\x1b[0m', stderr);
@@ -25,21 +25,24 @@ exec(`cd ${core.getInput('path')} && git fetch --tags --prune --unshallow -f`, (
             }
 
             tag = tag.trim()
+            exec(`cd ${core.getInput('path')} && git describe --abbrev=0 HEAD^`, (err, pastTag, stderr) => {
+                pastTag = pastTag.trim()
+                exec(`cd ${core.getInput('path')} && git log -1 --format=%at ${tag}`, (err, timestamp, stderr) => {
+                    if (err) {
+                        console.log('\x1b[33m%s\x1b[0m', 'Could not find any timestamp because: ');
+                        console.log('\x1b[31m%s\x1b[0m', stderr);
+                        process.exit(1);
+                    }
 
-            exec(`cd ${core.getInput('path')} && git log -1 --format=%at ${tag}`, (err, timestamp, stderr) => {
-                if (err) {
-                    console.log('\x1b[33m%s\x1b[0m', 'Could not find any timestamp because: ');
-                    console.log('\x1b[31m%s\x1b[0m', stderr);
-                    process.exit(1);
-                }
+                    timestamp = timestamp.trim()
 
-                timestamp = timestamp.trim()
-
-                console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
-                console.log('\x1b[32m%s\x1b[0m', `Found timestamp: ${timestamp}`);
-                console.log(`::set-output name=tag::${tag}`);
-                console.log(`::set-output name=timestamp::${timestamp}`);
-                process.exit(0);
+                    console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
+                    console.log('\x1b[32m%s\x1b[0m', `Found past tag: ${pastTag}`);
+                    console.log('\x1b[32m%s\x1b[0m', `Found timestamp: ${timestamp}`);
+                    console.log(`::set-output name=tag::${tag}`);
+                    console.log(`::set-output name=timestamp::${timestamp}`);
+                    process.exit(0);
+                });
             });
         });
     });
