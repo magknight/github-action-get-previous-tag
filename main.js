19040b022a0e2b9b939c296d274aafe51901ef2c
Alex Beavil <alex@aeroniemi.com>
2020-12-27 14:41:37 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 const core = require('@actions/core');
 
-exec(`cd ${core.getInput('path')} && git fetch --tags -f`, (err, res, stderr) => {
+exec(`cd ${core.getInput('path')} && git fetch --tags -all`, (err, res, stderr) => {
     if (err) {
         console.log('\x1b[33m%s\x1b[0m', `Couldn't fetch`);
         console.log('\x1b[31m%s\x1b[0m', stderr);
@@ -13,9 +13,7 @@ exec(`cd ${core.getInput('path')} && git fetch --tags -f`, (err, res, stderr) =>
             console.log('\x1b[31m%s\x1b[0m', stderr);
             process.exit(1);
         }
-
         rev = rev.trim()
-
         exec(`cd ${core.getInput('path')} && git describe --tags ${rev}`, (err, tag, stderr) => {
             if (err) {
                 console.log('\x1b[33m%s\x1b[0m', rev)
@@ -23,26 +21,15 @@ exec(`cd ${core.getInput('path')} && git fetch --tags -f`, (err, res, stderr) =>
                 console.log('\x1b[31m%s\x1b[0m', stderr);
                 process.exit(1);
             }
-
             tag = tag.trim()
             exec(`cd ${core.getInput('path')} && git describe --abbrev=0 HEAD^`, (err, pastTag, stderr) => {
                 pastTag = pastTag.trim()
-                exec(`cd ${core.getInput('path')} && git log -1 --format=%at ${tag}`, (err, timestamp, stderr) => {
-                    if (err) {
-                        console.log('\x1b[33m%s\x1b[0m', 'Could not find any timestamp because: ');
-                        console.log('\x1b[31m%s\x1b[0m', stderr);
-                        process.exit(1);
-                    }
+                console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
+                console.log('\x1b[32m%s\x1b[0m', `Found past tag: ${pastTag}`);
+                console.log(`::set-output name=tag::${tag}`);
+                console.log(`::set-output name=pastTag::${pastTag}`);
 
-                    timestamp = timestamp.trim()
-
-                    console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
-                    console.log('\x1b[32m%s\x1b[0m', `Found past tag: ${pastTag}`);
-                    console.log('\x1b[32m%s\x1b[0m', `Found timestamp: ${timestamp}`);
-                    console.log(`::set-output name=tag::${tag}`);
-                    console.log(`::set-output name=timestamp::${timestamp}`);
-                    process.exit(0);
-                });
+                process.exit(0);
             });
         });
     });
